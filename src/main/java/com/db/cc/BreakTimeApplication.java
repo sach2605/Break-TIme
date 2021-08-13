88b805a612ae11a17f4d74b2c0907c945c191068
Sachin Motwani <sachinmotwani26@gmail.com>
2021-08-13 12:31:39 +0530

    initial commit

---
+++ src/main/java/com/db/cc/BreakTimeApplication.java
@@ -0,0 +1,13 @@
+package com.db.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/cc/BreakTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.cc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
