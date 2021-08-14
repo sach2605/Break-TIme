afaeabc4b036e6e2264b1bfc2edb173927358634
Apurva021 <apurvadegreat@gmail.com>
2021-08-14 16:27:23 +0530

    implemented scheduled emails every two hours for office time

--- src/main/java/com/db/cc/BreakTimeApplication.java
+++ src/main/java/com/db/cc/BreakTimeApplication.java
@@ -2,8 +2,10 @@ package com.db.cc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BreakTimeApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/db/cc/emailnotification/ScheduleEmails.java
@@ -0,0 +1,103 @@
+package com.db.cc.emailnotification;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import javax.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.db.cc.model.Notifications;
+import com.db.cc.model.User;
+import com.db.cc.repo.NotificationsRepository;
+import com.db.cc.repo.PreferencesRepository;
+import com.db.cc.repo.UserRepository;
+
+@Component
+public class ScheduleEmails {
+	
+	@Autowired
+	private EmailSender emailSender;
+	
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private PreferencesRepository preferencesRepository;
+	
+	@Autowired
+	private NotificationsRepository notificationsRepository;
+	
+	private final long twohours = 3600 * 1000 * 2;
+	
+	
+	@Scheduled(fixedRate = twohours)
+	public void scheduleTaskWithFixedRate() throws ParseException {
+		
+		System.out.println("running scheduled task");
+		
+		LocalTime currentTime = LocalTime.now();
+		LocalTime nine = LocalTime.parse("09:00");
+		LocalTime eighteen = LocalTime.parse("18:00");
+		
+		try {
+			
+			if(currentTime.isAfter(nine) && currentTime.isBefore(eighteen)) {
+				System.out.println("can send email");
+				List<User> users = userRepository.findAll();
+				List<Notifications> notifications = notificationsRepository.findAll();
+				
+				
+				for(User user : users) {
+					
+					
+					
+					String preferences[] = preferencesRepository.findByUsername(user.getUsername()).getPreferences().split(",");
+					
+					List<String> notificationText = new ArrayList<>();
+					
+					for(String pref : preferences) {
+						
+						
+						for(Notifications notification: notifications) {
+							if(notification.getPreference().equalsIgnoreCase(pref) ) {
+								notificationText.add(notification.getNotificationMessage());
+							}
+						}
+					}
+					
+					Random random = new Random();
+					int index = random.nextInt(notificationText.size());
+					
+					try {
+						emailSender.sendSimpleEmail(user.getEmail(),
+								"codingchallengedb23@gmail.com",
+								"Notification",
+								notificationText.get(index));
+					} catch (MessagingException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+			
+		
+		
+	}
+	
+}
